cd1b341204a047d681921abca473a221f7cc9379
mikko.ronkkomaki <mikko.ronkkomaki@solita.fi>
2024-03-18 14:51:19 +0200

    Add some test data

--- database/testdata/tasks.sql
+++ database/testdata/tasks.sql
@@ -1,3 +1,11 @@
-INSERT INTO TASK(description) VALUES ('Ranteet auki ja Ranualle');
-INSERT INTO TASK(description) VALUES ('Notta uhraappas Sebekille');
-INSERT INTO TASK(description) VALUES ('Mummu Tiamat vanhainkotiin');
+INSERT INTO task (description) VALUES ('Piippolan vaarilla oli Blashyrkh');
+INSERT INTO task (description) VALUES ('Ken aasilla ratsastaan, Barbi mursulla');
+INSERT INTO task (description) VALUES ('Herra Sebaot, sitä kissa ostaisi');
+INSERT INTO task (description) VALUES ('Ollappa nuori ja ropotti');
+INSERT INTO task (description) VALUES ('Tottua saan / Koodailemaan');
+INSERT INTO task (description) VALUES ('Hilipatipippaa');
+INSERT INTO task (description) VALUES ('Sillivati seilaa');
+INSERT INTO task (description) VALUES ('Paska haisee ja balalaikka soi');
+INSERT INTO task (description) VALUES ('Kettu on hevoiseni');
+INSERT INTO task (description) VALUES ('Mummu Tiamat vanhainkotiin');
+INSERT INTO task (description) VALUES ('Notta uhrataampas Sebekkiin!');
