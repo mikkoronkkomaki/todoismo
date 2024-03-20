a0f504fd2bf75262abf8dc73793a851bc7b5d2e4
mikko.ronkkomaki <mikko.ronkkomaki@solita.fi>
2024-03-20 12:29:29 +0200

    Add a button to clear the search text

--- frontend/src/search.svelte
+++ frontend/src/search.svelte
@@ -10,11 +10,16 @@
         newUrl.searchParams.set('currentPage', "0");
         goto(newUrl.toString());
     };
+
+    const clearInput = () => {
+        query = '';
+    };
 </script>
 
 <h5>Search tasks</h5>
 
 <form class="d-flex justify-content-between" on:submit|preventDefault={search}>
     <input type="text" bind:value={query} placeholder="Search tasks" class="form-control flex-grow">
+    <button on:click={clearInput} class="btn btn-warning ms-2 no-wrap btn-width">Clear</button>
     <button type="submit" class="btn btn-primary ms-2 no-wrap btn-width">Search</button>
 </form>
